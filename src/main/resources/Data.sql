0e18213cda57bb68a0eb8708e08d6df7b517822c
leonardo <demetrio.vd@hotmail.com>
2022-02-02 07:13:55 -0300

    Readme

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,8 +1,8 @@
-insert into games (nome, imagem, preco, secao_id) values ('God of war','Assets/img/americanas/god of war.webp', 99.90,2);
-insert into games (nome, imagem, preco, secao_id) values ('The last of us 2', 'Assets/img/americanas/thelast.webp', 135.90,2);
-insert into games (nome, imagem, preco, secao_id) values ('Watch Dogs Legion', 'Assets/img/americanas/whatdogs legion.webp', 39.90,1);
-insert into games (nome, imagem, preco, secao_id) values ('Forza Horizon 5', 'Assets/img/americanas/forza.webp', 159.90,1);
-insert into games (nome, imagem, preco, secao_id) values ('Dying Light','Assets/img/americanas/dying light.webp',39.90, 2);
-insert into games (nome, imagem, preco, secao_id) values ('CyberPunk', 'Assets/img/americanas/cyberPunk.webp', 39.90, 2);
-insert into games (nome, imagem, preco, secao_id) values ('BF 2042 - PS5', 'Assets/img/americanas/bf2042ps5.webp', 179.91, 2);
-insert into games (nome, imagem, preco, secao_id) values ('Days Gone', 'Assets/img/americanas/daysgone.webp', 59.80, 2);
+insert into games (nome, imagem, preco, secao_id) values ('God of war','Assets/img/webp/god of war.webp', 99.90,2);
+insert into games (nome, imagem, preco, secao_id) values ('The last of us 2', 'Assets/img/webp/thelast.webp', 135.90,2);
+insert into games (nome, imagem, preco, secao_id) values ('Watch Dogs Legion', 'Assets/img/webp/whatdogs legion.webp', 39.90,1);
+insert into games (nome, imagem, preco, secao_id) values ('Forza Horizon 5', 'Assets/img/webp/forza.webp', 159.90,1);
+insert into games (nome, imagem, preco, secao_id) values ('Dying Light','Assets/img/webp/dying light.webp',39.90, 2);
+insert into games (nome, imagem, preco, secao_id) values ('CyberPunk', 'Assets/img/webp/cyberPunk.webp', 39.90, 2);
+insert into games (nome, imagem, preco, secao_id) values ('BF 2042 - PS5', 'Assets/img/webp/bf2042ps5.webp', 179.91, 2);
+insert into games (nome, imagem, preco, secao_id) values ('Days Gone', 'Assets/img/webp/daysgone.webp', 59.80, 2);
